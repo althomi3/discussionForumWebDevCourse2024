0bd2aaee930ab2df26422725b1039bdd89e04e77
althomi3 <alischa.thomas@aalto.fi>
2025-01-05 12:23:21 +0100

    adds routing for /courses with POST request

--- server/app.js
+++ server/app.js
@@ -8,10 +8,10 @@ const app = new Hono(); // instantiates hono object from hono web framework
 
 // ---PROJECT IMPLEMENTATIONN SERVER SIDE---
 
-// ROUTING LANDING PAGE
+// A: ROUTING LANDING PAGE
 app.get("/", (c) => c.text("Hello"));
 
-// ROUTING /COURSES PATH
+// ROUTING GET REQUEST / /COURSES PATH
 // client queries server with GET request, server returns the following JSON document
 // '{"courses": [ {"id": 1, "name": "Web Software Development" }, {"id": 2, "name": "Device-Agnostic Design" } ] }'
 app.get("/courses", (c) => {
@@ -19,17 +19,32 @@ app.get("/courses", (c) => {
     return c.json(data);
 });
 
-// ROUTING /COURSES/:ID
+// B: ROUTING / GET REQUEST / /COURSES/:ID
 // client queries server on path /courses/:id
 // server returns a JSON document:
 //'{"course": {"id": :id, "name": "Course Name" } }'
 // The name is a string (just "Course Name", not e.g. "Web Software Development",
 //':id' is retrieved from the path and is a number).
-
 app.get("/courses/:id", (c) => {
     const id = c.req.param("id");
     const data = {"course": {"id": `${id}`, "name": "Course Name" }}
     return c.json(data);
 });
 
+// C: POST REQUEST / /COURSES
+// clients query server to /courses path and send json data with post request
+    // body contains a JSON document that looks as follows: '{"name": "Course Name" }'
+// server returns a JSON document:  
+    // returns a JSON document that looks as follows: '{"course": {"id": 3, "name": "Course Name" } }'. 
+    // The value for "id" is always 3, but the name of the course should be extracted from the request.
+
+app.post("/courses", async (c) => {
+    const post_data = await c.req.json(); // retrieves json data from http request body
+    const course_name = post_data.course.name; // accesses course name property
+    const return_data = {"course": {"id":3, "name": `${course_name}`}} // defined json data for response
+    return c.json(return_data); // returns json response
+});
+
+// D: 
+// GET /courses/:id/topics, where ':id' is a path variable, returns a JSON document that looks as follows: '{"topics": [ { "id": 1, "name": "Topic 1" }, {"id": 2, "name": "Topic 2" } ] }'. Nothing is done with the path variable.
 export default app;
